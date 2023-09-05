ea998b207b70a405079e999ea5b4c328a553a276
Igor Suzuki Kira <89170220+igorsuzuki99@users.noreply.github.com>
2023-09-04 22:05:56 -0300

    add initial files - spring initializr

---
+++ kitchencloud/src/main/java/com/carcara/oracle/kitchencloud/KitchencloudApplication.java
@@ -0,0 +1,13 @@
+package com.carcara.oracle.kitchencloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchencloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchencloudApplication.class, args);
+	}
+
+}
---
+++ kitchencloud/src/test/java/com/carcara/oracle/kitchencloud/KitchencloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carcara.oracle.kitchencloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchencloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
