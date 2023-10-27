321ec961b6c417581f2d2a68404f84f71d73fd27
Alan Araujo Rodrigues <alanaraujotrs@gmail.com>
2023-10-26 21:44:12 -0300

    CCC-87 teste

--- kitchencloud/src/main/java/com/carcara/oracle/kitchencloud/service/FuncionarioService.java
+++ kitchencloud/src/main/java/com/carcara/oracle/kitchencloud/service/FuncionarioService.java
@@ -7,7 +7,6 @@ import com.carcara.oracle.kitchencloud.repository.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,8 +15,6 @@ public class FuncionarioService {
 
     @Autowired
     private FuncionarioRepository funcionarioRepository;
-    @Autowired
-    private FolgaRepository folgaRepository;
 
     public List<ExibicaoFuncionarioDTO> listarFuncionarios(){
         List<Funcionario> funcionarios = funcionarioRepository.findAll();
